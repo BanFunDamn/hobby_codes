6cdbecfccaef24ffa94b70cbedc52b1b9e7c0b94
BanFunDamn <luckyhappening777@gmail.com>
2020-11-25 13:18:43 +0900

    update

--- activate.sh
+++ activate.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
-python3 check_niconico_update.py
+niconico="niconico.html"
+
+git pull >& /dev/null
+
+python3 check_niconico_update.py -o $niconico
 python3 auto_open_manga_site.py
+
+git add niconico.html
+git add log
+git commit -m "log updated" >& /dev/null
+git push origin master >& /dev/null
